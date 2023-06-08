2e5f4761fd0d2e5fe4243b20805f3ee0d5a310a4
Katherine Blaine <blaine.katherine12@gmail.com>
2023-06-08 08:41:36 -0500

    feat: Connect mock data to article details route

--- src/Components/ArticleDetails/ArticleDetails.js
+++ src/Components/ArticleDetails/ArticleDetails.js
@@ -1,8 +1,22 @@
+import { sampleArticle } from '../../SampleData/sampleArticle';
+import { useState, useEffect } from 'react';
 import './ArticleDetails.css'
 
 const ArticleDetails = () => {
+  const [article, setArticle] = useState({})
+
+  useEffect(() => {
+    setArticle(sampleArticle)
+  }, [])
+
   return (
-    <h1>Article Details</h1>
+    <div className='details'>
+      <h1>Article Details</h1>
+      <h2>{article.title}</h2>
+      <img src={article.urlToImage}></img>
+      <a href={article.url}>{article.url}</a>
+      <p>{article.content}</p>
+    </div>
   )
 }
 
